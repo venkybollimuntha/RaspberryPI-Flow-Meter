740c2e3d7dd7f3826ebf58e38e5de3b8d0de22fb
Venky Bollimuntha <35519461+venkybollimuntha@users.noreply.github.com>
2019-07-24 20:22:06 +0530

    working code final

--- flow_meter.py
+++ flow_meter.py
@@ -10,20 +10,30 @@ import datetime
 from fpdf import FPDF
 import sys
 import sqlite3
+import csv
+import pandas as pd
+import xlsxwriter 
+
+from apscheduler.schedulers.background import BackgroundScheduler
 
 app = Flask(__name__)
+
+conn = sqlite3.connect('flow_meter.db')
+conn.execute("CREATE TABLE IF NOT EXISTS sensor_data ( id INTEGER PRIMARY KEY AUTOINCREMENT, value REAL, datetime_val TEXT )")
+conn.execute("CREATE TABLE IF NOT EXISTS user_auth ( id INTEGER PRIMARY KEY AUTOINCREMENT, username TEXT, password VARCHAR(50),dob TEXT,email VARCHAR(50),role VARCHAR(30),user_created_date TEXT)")
+conn.commit()
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///flow_meter.db'
 app.secret_key = 'XP)(OIUip08u7yhg8'
 
-app.config.update(
-    DEBUG=False,
-    MAIL_SERVER='smtp.gmail.com',
-    MAIL_PORT=465,
-    MAIL_USE_SSL=True,
-    MAIL_USERNAME='doubts2venky@gmail.com',
-    MAIL_PASSWORD='bhagi@956'
-)
+# app.config.update(
+#     DEBUG=False,
+#     MAIL_SERVER='smtp.gmail.com',
+#     MAIL_PORT=465,
+#     MAIL_USE_SSL=True,
+#     MAIL_USERNAME='doubts2venky@gmail.com',
+#     MAIL_PASSWORD='bhagi@956'
+# )
 # mail = Mail(app)
 db = SQLAlchemy(app)
 db.Model.metadata.reflect(db.engine)
@@ -34,9 +44,9 @@ login = LoginManager(app)
 # Each time the logged-in user navigates to a new page, it retrieves the ID of the user from the session, and then loads that user into memory.
 @login.user_loader
 def load_user(user_id):
-    return User.query.get(user_id)
+    return user_auth.query.get(user_id)
 
-class User(db.Model,UserMixin):
+class user_auth(db.Model,UserMixin):
     __table_args__ = {'extend_existing': True}
 
     id = db.Column(db.Integer, primary_key = True)
@@ -48,7 +58,7 @@ class User(db.Model,UserMixin):
 
 class MyModelView(ModelView):
     def is_accessible(self):
-        user = User.query.get(current_user.get_id())
+        user = user_auth.query.get(current_user.get_id())
         roles_list = ['SuperAdmin', 'Engineer', 'Manager']
         if current_user.is_authenticated and user.role in roles_list:
             return True # if True table is visible in Admin page
@@ -56,17 +66,17 @@ class MyModelView(ModelView):
             return True # if False table is not visible in Admin page
 
 admin = Admin(app, name='RPI')
-admin.add_view(MyModelView(User, db.session))
+admin.add_view(MyModelView(user_auth, db.session))
 
 @app.route('/',methods=['GET','POST'])
 def landing_page():
     data = {}
     if current_user.is_authenticated:
-        user = User.query.get(current_user.get_id())
+        user = user_auth.query.get(current_user.get_id())
         data['role'] = user.role
         data['role_list'] = ['SuperAdmin', 'Engineer','Manager']
         data['user'] = current_user.is_authenticated
-        return render_template('main_layout.html', data = data)
+        return render_template('index.html', data = data)
     else:
         return render_template('login.html', data = data)
 
@@ -77,7 +87,7 @@ def login():
         username = request.form['username']
         password = request.form['password']
         print(username,password)
-        user = User.query.filter_by(username=username).first()
+        user = user_auth.query.filter_by(username=username).first()
         if user:
             if check_password_hash(user.password, password):
                 login_user(user)
@@ -106,10 +116,10 @@ def signup():
         email = request.form['email']
         dob = request.form['dob']
         print(username,password, email)
-        user = User.query.filter_by(email=email).first()
+        user = user_auth.query.filter_by(email=email).first()
         if not user:
             print(str(username)+' : does not exist!!')
-            new_user = User(email=email, username=username, password=generate_password_hash(password, method='sha256'),dob=dob)
+            new_user = user_auth(email=email, username=username, password=generate_password_hash(password, method='sha256'),dob=dob)
             db.session.add(new_user)
             db.session.commit()
             print(str(username)+' : successfully created!!')
@@ -139,7 +149,7 @@ def reset():
     data= {}
     if request.method == 'GET' and first == 'true':
 
-        user = User.query.filter_by(username=username,dob = dob).first()
+        user = user_auth.query.filter_by(username=username,dob = dob).first()
         print(user,dob,username)
         if user:
             data['is_success'] = True
@@ -158,7 +168,7 @@ def reset():
         print('password-------------------',password)
         print('username-------------------',username)
         if username:
-            user=User.query.filter(User.username==username).first()
+            user=user_auth.query.filter(user_auth.username==username).first()
             user.password = generate_password_hash(password, method='sha256')
             db.session.commit()
             print('updated successfully')
@@ -171,7 +181,7 @@ def reset():
 def live():
     conn = sqlite3.connect('flow_meter.db')
     cursor = conn.cursor()
-    d = 'SELECT * FROM flow_meter_example order by id desc limit 1'
+    d = 'SELECT * FROM sensor_data order by id desc limit 1'
     print(d)
     cursor.execute(d)
     rec= cursor.fetchall()
@@ -199,7 +209,7 @@ def date_filter_query(pick_val,selected_day =None):
 
         conn = sqlite3.connect('flow_meter.db')
         cursor = conn.cursor()
-        query = f"SELECT distinct id,AVG(value),datetime_val FROM flow_meter_example where datetime_val BETWEEN '{start_date}' and '{end_date}' GROUP BY strftime('%d', datetime_val);"
+        query = f"SELECT distinct id,AVG(value),datetime_val FROM sensor_data where datetime_val BETWEEN '{start_date}' and '{end_date}' GROUP BY strftime('%d', datetime_val);"
         print(query)
         cursor.execute(query)
         result = cursor.fetchall()
@@ -211,7 +221,7 @@ def date_filter_query(pick_val,selected_day =None):
             cursor = conn.cursor()
             start_date = str(choosed_date)[0:10] + ' 00:00:00'
             end_date = str(choosed_date)[0:10] + ' 23:59:59'
-            query = f"SELECT distinct id,AVG(value),datetime_val FROM flow_meter_example where datetime_val BETWEEN '{start_date}' and '{end_date}' GROUP BY strftime('%H', datetime_val);"
+            query = f"SELECT distinct id,AVG(value),datetime_val FROM sensor_data where datetime_val BETWEEN '{start_date}' and '{end_date}' GROUP BY strftime('%H', datetime_val);"
             cursor.execute(query)
             result = cursor.fetchall()
     return result
@@ -253,70 +263,122 @@ def filter():
             return json.dumps({"id_list":id_list,"flow_list":flow_list,"time_list":time_list})
 
 
-@app.route('/pdf', methods=['GET','POST'])
-def pdf_data():
-    f1=request.args.get('f1')
-    f2=request.args.get('f2')
-    result = date_filter_query(f1,f2)
+def download_file():
+    print('venkybollimuntha')
 
-    print(',,----------------,,',result)
-    get_pdf = True if request.args.get('get_pdf') == 'true' else False
-    send_mail = True if request.args.get('send_mail') == 'true' else False
-    print('get_pdf',get_pdf,'send_mail',send_mail)
 
-    if get_pdf:
-        download_pdf(result)
+# @app.route('/pdf', methods=['GET','POST'])
+# def pdf_data():
+#     f1=request.args.get('f1')
+#     f2=request.args.get('f2')
+#     result = date_filter_query(f1,f2)
 
-    if send_mail:
-        mail_pdf()
-    return 'called'
+#     print(',,----------------,,',result)
+#     get_pdf = True if request.args.get('get_pdf') == 'true' else False
+#     send_mail = True if request.args.get('send_mail') == 'true' else False
+#     print('get_pdf',get_pdf,'send_mail',send_mail)
 
-# When user click on the download button this method is called
-def download_pdf(data_list):
-    try:
-        print('download PDF method called')
-        # adding database records to a pdf_data_list
-        pdf_data_list = [['ID', 'Flow Rate', 'Timestamp']]
-        for rec in data_list:
-            res = list(rec)
-            pdf_data_list.append(res)
+#     if get_pdf:
+#         download_pdf(result)
 
-        # adding data to PDF file
-        pdf = FPDF()
-        pdf.add_page()
-        pdf.set_font("Arial", size=10)
-        col_width = pdf.w / 4.0
-        row_height = pdf.font_size
-        for row in pdf_data_list:
-            for item in row:
-                pdf.cell(col_width, row_height*2,txt=item, border=1)
-            pdf.ln(row_height*2)
-        pdf.output("simple_demo.pdf")
-    except Exception as e:
-        print('Error on line {}'.format(sys.exc_info()[-1].tb_lineno))
-        print('Exception occured in download_pdf method: '+str(e))
-        return('Exception occured in download_pdf method: '+str(e)) 
+#     if send_mail:
+#         mail_pdf()
+#     return 'called'
+
+# # When user click on the download button this method is called
+# def download_pdf():
+#     try:
+#         print('download PDF method called')
+#         # adding database records to a pdf_data_list
+#         pdf_data_list = [['ID', 'Flow Rate', 'Timestamp']]
+#         for rec in data_list:
+#             res = list(rec)
+#             pdf_data_list.append(res)
+
+#         # adding data to PDF file
+#         pdf = FPDF()
+#         pdf.add_page()
+#         pdf.set_font("Arial", size=10)
+#         col_width = pdf.w / 4.0
+#         row_height = pdf.font_size
+#         for row in pdf_data_list:
+#             for item in row:
+#                 pdf.cell(col_width, row_height*2,txt=item, border=1)
+#             pdf.ln(row_height*2)
+#         pdf.output("simple_demo.pdf")
+#     except Exception as e:
+#         print('Error on line {}'.format(sys.exc_info()[-1].tb_lineno))
+#         print('Exception occured in download_pdf method: '+str(e))
+#         return('Exception occured in download_pdf method: '+str(e)) 
 
 
 # sending attachment as pdf to the user
-def mail_pdf():
-    try:
-        receiver_email = ["venkybollimuntha@gmail.com"]
-        sender_email = "doubts2venky@gmail.com"
-        password = 'bhagi@956'
-        mail_body = 'Please find the below attached file'
-        subject = 'We added PDF as a attachment'
-        msg = Message(subject=subject,sender=sender_email,recipients=receiver_email)
-        msg.body = mail_body
-        with app.open_resource("simple_demo.pdf",'rb') as fp:
-            msg.attach("simple_demo.pdf","application/pdf", fp.read())
-        mail.send(msg)
-        print('Maiil sent successfully')
-    except Exception as e:
-        print('Error on line {}'.format(sys.exc_info()[-1].tb_lineno))
-        print('Exception occured in mail_pdf method: '+str(e))
-        return('Exception occured in mail_pdf method: '+str(e))
+# def mail_pdf():
+#     try:
+#         receiver_email = ["venkybollimuntha@gmail.com"]
+#         sender_email = "doubts2venky@gmail.com"
+#         password = 'bhagi@956'
+#         mail_body = 'Please find the below attached file'
+#         subject = 'We added PDF as a attachment'
+#         msg = Message(subject=subject,sender=sender_email,recipients=receiver_email)
+#         msg.body = mail_body
+#         with app.open_resource("simple_demo.pdf",'rb') as fp:
+#             msg.attach("simple_demo.pdf","application/pdf", fp.read())
+#         mail.send(msg)
+#         print('Maiil sent successfully')
+#     except Exception as e:
+#         print('Error on line {}'.format(sys.exc_info()[-1].tb_lineno))
+#         print('Exception occured in mail_pdf method: '+str(e))
+#         return('Exception occured in mail_pdf method: '+str(e))
+
+
+
+def download_file():
+    print('called')
+    import calendar
+    import datetime
+    month = 8#datetime.date.today().month 
+    year = 2019#datetime.date.today().year
+    if month == 1:
+        month = 12
+        year -=1 
+    else:
+        month -= 1
+    if month in list(range(1,10)):
+        month= f'{0}{month}'
+
+    last_day = calendar.monthrange(year,int(month))[1]
+    conn = sqlite3.connect('flow_meter.db')
+    cursor = conn.cursor()
+    start_date = f'{year}-{month}-01 00:00:00'
+    end_date = f'{year}-{month}-{last_day} 23:59:59'
+    query = f"SELECT * FROM sensor_data where datetime_val BETWEEN '{start_date}' and '{end_date}'"
+    cursor.execute(query)
+    result = cursor.fetchall()
+    #print(result)
+    dataFrame = {'Id': [], 'Value': [], 'Date': []}
+    
+    for row in cursor.execute(query):
+
+        dataFrame['Id'].append(row[0])
+        dataFrame['Value'].append(row[1])
+        dataFrame['Date'].append(row[2])
+    
+    sheet_name = datetime.datetime.utcnow().strftime('%B - %Y') 
+    dataExport = pd.DataFrame(dataFrame)
+    dataToExcel = pd.ExcelWriter(f'{sheet_name}.xlsx', engine='xlsxwriter')
+    dataExport.to_excel(dataToExcel, sheet_name=sheet_name, columns=['Id', 'Value', 'Date'])
+    dataToExcel.save()
+    print('written data successfully')
+
+
+def v():
+    print('sdfghjsdfghjdfghj')
 
 
 if __name__ == '__main__':
+    scheduler = BackgroundScheduler()
+    scheduler.add_job(download_file, 'cron', month='1-12', day=24, hour=18,minute=45)
+    scheduler.start()
     app.run(debug=True)
+ 
